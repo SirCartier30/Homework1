80057314f158218f34b0468ed02b8d03cfeb3cef
brewski <oomotere1@ggc.edu>
2023-02-02 12:13:18 -0500

    initial commit

---
+++ SpeedLight1.java
@@ -0,0 +1,18 @@
+/**
+ * @class: Speed Light
+ * @author: Daniel Omotere
+ * @course: ITEC 2140-04, Spring 2023
+ * @written: January 28, 2023
+ * @description: This pr
+ */
+public class SpeedLight {
+    public static void main (String [] args){
+
+        int SPEED_OF_LIGHT= 340;
+        int timeElapsed= 5;
+        int distance= SPEED_OF_LIGHT * timeElapsed;
+System.out.println("Speed of Sound: " + SPEED_OF_LIGHT);
+System.out.println("Time Elapsed: " + timeElapsed);
+System.out.println("Distance:" + SPEED_OF_LIGHT * timeElapsed);
+
+}
